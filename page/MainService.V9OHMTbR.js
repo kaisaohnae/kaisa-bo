3bca2a19c5fb55e3f04437d1d6b4c39856f2aa96
kaisaohnae <kaisaohnae@users.noreply.github.com>
2024-12-02 05:39:07 +0000

    deploy: 361f43849519f79cb57f0d4c9abeebf4bfe17f06

--- page/MainService.V9OHMTbR.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./apiConfig.FeihSf0O.js";class s{async dashboard(a){return(await r("/common/dashboard",a||{})).data}async calendar(a){return(await r("/common/calendar",a||{})).data}}const c=new s;export{c as M};
