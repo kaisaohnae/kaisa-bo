a2ae66684c91194180e3308067d8d5c3007c9964
kaisaohnae <kaisaohnae@users.noreply.github.com>
2024-12-24 02:19:13 +0000

    deploy: db88f078532ad9b77d7d64ee17e586aedd8de231

--- page/MainService.Bbliw61Y.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./apiConfig.Cb5X9RbM.js";class s{async dashboard(a){return(await r("/common/dashboard",a||{})).data}async calendar(a){return(await r("/common/calendar",a||{})).data}}const c=new s;export{c as M};
