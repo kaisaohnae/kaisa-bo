cf953b01dbf5e55051e7a3b798545334256fe80b
kaisaohnae <kaisaohnae@users.noreply.github.com>
2025-02-17 00:56:14 +0000

    deploy: ebe222041f9085b7a9ceed1aa238a8b8867ca38d

--- page/main-service.BKDSSNc-.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./api-config.DUn_w41-.js";class s{async dashboard(a){return(await r("/common/dashboard",a||{})).data}async calendar(a){return(await r("/common/calendar",a||{})).data}}const c=new s;export{c as M};
---
+++ page/main-service.CYjz4C43.js
@@ -0,0 +1,1 @@
+import{a as r}from"./api-config.DDt0lwuS.js";class s{async dashboard(a){return(await r("/common/dashboard",a||{})).data}async calendar(a){return(await r("/common/calendar",a||{})).data}}const c=new s;export{c as M};
